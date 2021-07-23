a4b9e0eb744fbebf27a6da3699ba00b57d0b9a1e
Audrey Copeland <acopeland@bandwidth.com>
2021-07-23 14:39:09 -0400

    EDGE-630 Remove type hinting

--- bandwidth/webrtc/utils/transfer_util.py
+++ bandwidth/webrtc/utils/transfer_util.py
@@ -6,7 +6,7 @@ Method to generate the transfer BXML to connect WebRTC <-> phones
 @copyright Bandwidth INC
 """
 
-def generate_transfer_bxml(device_token: str, voice_call_id: str, sip_uri: str ='sip:sipx.webrtc.bandwidth.com:5060'):
+def generate_transfer_bxml(device_token, voice_call_id, sip_uri='sip:sipx.webrtc.bandwidth.com:5060'):
     """
     Returns BXML string with WebRTC a device token to perform a SIP transfer
     :param device_token: The device token
@@ -15,7 +15,7 @@ def generate_transfer_bxml(device_token: str, voice_call_id: str, sip_uri: str =
     """
     return '<?xml version="1.0" encoding="UTF-8"?><Response>' + generate_transfer_bxml_verb(device_token, voice_call_id, sip_uri) + '</Response>'
 
-def generate_transfer_bxml_verb(device_token: str, voice_call_id: str, sip_uri: str ='sip:sipx.webrtc.bandwidth.com:5060'):
+def generate_transfer_bxml_verb(device_token, voice_call_id, sip_uri='sip:sipx.webrtc.bandwidth.com:5060'):
     """
     Returns the Transfer verb to perform the SIP transfer
     :param device_token: The device token
