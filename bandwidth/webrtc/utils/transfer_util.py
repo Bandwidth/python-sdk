7fea2c990d4e673a794cfeeb53bb793e77183dfd
Audrey Copeland <acopeland@bandwidth.com>
2021-07-23 08:34:11 -0400

    EDGE-630 Add voice call id to the transfer

--- bandwidth/webrtc/utils/transfer_util.py
+++ bandwidth/webrtc/utils/transfer_util.py
@@ -6,14 +6,14 @@ Method to generate the transfer BXML to connect WebRTC <-> phones
 @copyright Bandwidth INC
 """
 
-def generate_transfer_bxml(device_token, sip_uri='sip:sipx.webrtc.bandwidth.com:5060'):
+def generate_transfer_bxml(device_token: str, voice_call_id: str, sip_uri: str ='sip:sipx.webrtc.bandwidth.com:5060'):
     """
     Returns BXML string with WebRTC a device token to perform a SIP transfer
     """
-    return '<?xml version="1.0" encoding="UTF-8"?><Response>' + generate_transfer_bxml_verb(device_token, sip_uri) + '</Response>'
+    return '<?xml version="1.0" encoding="UTF-8"?><Response>' + generate_transfer_bxml_verb(device_token, voice_call_id, sip_uri) + '</Response>'
 
-def generate_transfer_bxml_verb(device_token, sip_uri='sip:sipx.webrtc.bandwidth.com:5060'):
+def generate_transfer_bxml_verb(device_token: str, voice_call_id: str, sip_uri: str ='sip:sipx.webrtc.bandwidth.com:5060'):
     """
     Returns the Transfer verb to perform the SIP transfer
     """
-    return f'''<Transfer><SipUri uui="{device_token};encoding=jwt">{sip_uri}</SipUri></Transfer>'''
+    return f'''<Transfer><SipUri uui="{voice_call_id};encoding=base64,{device_token};encoding=jwt">{sip_uri}</SipUri></Transfer>'''
