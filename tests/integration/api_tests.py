95e3050ba3f009195d025a9274ecb7e8cc96002a
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-09-13 09:20:21 -0400

    Add import statement

--- tests/integration/api_tests.py
+++ tests/integration/api_tests.py
@@ -10,6 +10,7 @@ from bandwidth.messaging.exceptions.messaging_exception import MessagingExceptio
 from bandwidth.voice.exceptions.api_error_exception import ApiErrorException
 from bandwidth.messaging.models.message_request import MessageRequest
 from bandwidth.voice.models.create_call_request import CreateCallRequest
+from bandwidth.voice.models.machine_detection_request import MachineDetectionRequest
 from bandwidth.multifactorauth.models.two_factor_code_request_schema import TwoFactorCodeRequestSchema
 from bandwidth.multifactorauth.models.two_factor_verify_request_schema import TwoFactorVerifyRequestSchema
 from bandwidth.phonenumberlookup.models.order_request import OrderRequest
