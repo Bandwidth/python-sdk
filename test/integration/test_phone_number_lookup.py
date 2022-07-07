6962d281f94139cfd253b0d562c8772f0e993557
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-07-07 12:35:06 -0400

    Regenerate so that createLookupResponse returns enum for status

--- test/integration/test_phone_number_lookup.py
+++ test/integration/test_phone_number_lookup.py
@@ -10,6 +10,7 @@ from bandwidth.api import phone_number_lookup_api
 from bandwidth.model.lookup_request import LookupRequest
 from bandwidth.model.create_lookup_response import CreateLookupResponse
 from bandwidth.model.lookup_status import LookupStatus
+from bandwidth.model.lookup_status_enum import LookupStatusEnum
 from bandwidth.model.tn_lookup_request_error import TnLookupRequestError
 
 
@@ -39,8 +40,10 @@ class TestPhoneNumberLookupIntegration(unittest.TestCase):
             ],
         ) 
 
-        create_lookup_response: CreateLookupResponse = api_instance.create_lookup(self.account_id, lookup_request)
-        get_lookup_status_response: LookupStatus = api_instance.get_lookup_status(self.account_id, create_lookup_response.request_id)
+        create_lookup_response: CreateLookupResponse = api_instance.create_lookup(self.account_id, lookup_request, _return_http_data_only=False)
+        self.assertEqual(202, create_lookup_response[1])
+        self.assertIs(LookupStatusEnum, type(create_lookup_response[0].status))
+        get_lookup_status_response: LookupStatus = api_instance.get_lookup_status(self.account_id, create_lookup_response[0].request_id)
     
     
     def testFailedPhoneNumberLookup(self):
