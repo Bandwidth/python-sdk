edce9d0e3deedb4aafc7863dc95f1eecccaa3bf7
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-07-14 13:47:40 -0400

    Add Media Integration Tests

--- test/integration/test_media_api.py
+++ test/integration/test_media_api.py
@@ -3,13 +3,16 @@ Integration test for Bandwidth's Multi-Factor Authentication API
 """
 
 import os
+import filecmp
 import unittest
+
+from rich import inspect
 
 import bandwidth
 from bandwidth.api import media_api
+from bandwidth.model.media import Media
 
-
-class TestMultiFactorAuthentication(unittest.TestCase):
+class TestMedia(unittest.TestCase):
     """Media API integration Test
     """
 
@@ -23,25 +26,75 @@ class TestMultiFactorAuthentication(unittest.TestCase):
         self.account_id = os.environ['BW_ACCOUNT_ID']
         self.media_path = "./test/fixtures/"
         self.media_id = "python_cat.jpeg"
+        self.download_file_path = "cat_download.jpeg"
+
+        self.original_file = open(self.media_path + self.media_id, "rb")
+    
+    def tearDown(self) -> None:
+        self.original_file.close()
 
     def testUploadMedia(self):
         media_id = self.media_id
         content_type = "image/jpeg"
         cache_control = "no-cache"
-        image_file = open(self.media_path + self.media_id, "rb")
 
-        api_response = self.api_instance.upload_media(
+        api_response_with_http_info = self.api_instance.upload_media(
             account_id=self.account_id,
             media_id=media_id,
-            body=image_file,
+            body=self.original_file,
             content_type=content_type,
-            cache_control=cache_control
+            cache_control=cache_control,
+            _return_http_data_only=False
         )
 
-    def testDownloadMedia(self):
+        self.assertEqual(api_response_with_http_info[1], 204)
+
+        # returns void
+        self.api_instance.upload_media(
+            account_id=self.account_id,
+            media_id=media_id,
+            body=self.original_file,
+            content_type=content_type,
+            cache_control=cache_control,
+            _return_http_data_only=False
+        )
+
+    def testGetMedia(self):
+        api_response_with_http_info = self.api_instance.get_media(
+            self.account_id, self.media_id, _return_http_data_only=False)
+        
+        self.assertEqual(api_response_with_http_info[1], 200)
+        
         api_response = self.api_instance.get_media(
             self.account_id, self.media_id, _preload_content=False)
         
-        downloadFile = open(self.media_path + "new_file_download.jpeg", "wb")
-        downloadFile.write(api_response.data)
-        downloadFile.close()
+        download_file = open(self.media_path + self.download_file_path, "wb")
+        download_file.write(api_response.data)
+
+        self.assertTrue(filecmp.cmp(self.media_path + self.media_id, self.media_path + self.download_file_path))
+        download_file.close()
+    
+    def testListMedia(self):
+        api_response_with_http_info = self.api_instance.list_media(
+            self.account_id, _return_http_data_only=False)
+        
+        self.assertEqual(api_response_with_http_info[1], 200)
+
+        api_response = self.api_instance.list_media(self.account_id)
+        
+        self.assertIs(type(api_response[0]), Media)
+
+    def testDeleteMedia(self):
+        api_response_with_http_info = self.api_instance.delete_media(
+            self.account_id, self.media_id, _return_http_data_only=False)
+        
+        self.assertEqual(api_response_with_http_info[1], 204)
+        pass
+
+    def testGetMediaWithBandwidthId(self): 
+        media_id = "c0e41604-9a21-4b68-9289-f47c98856e87/1/image000000.jpg"
+
+        api_response = self.api_instance.get_media(
+            self.account_id, media_id, _preload_content=False)
+        
+        inspect(api_response)
