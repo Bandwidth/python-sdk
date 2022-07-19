7c3926d3fbb329129b6f061a747fcde65658094b
Cameron Koegel <ckoegel@bandwidth.com>
2022-07-19 17:14:43 -0400

    updates based on comments

--- test/integration/test_messages_api.py
+++ test/integration/test_messages_api.py
@@ -13,7 +13,6 @@
 import os
 import json
 import unittest
-from array import array
 from datetime import datetime
 
 import bandwidth
@@ -36,6 +35,7 @@ class TestMessagesApi(unittest.TestCase):
     """MessagesApi unit test stubs"""
 
     def setUp(self):
+        # API Client
         configuration = bandwidth.Configuration(
             username = os.environ.get('BW_USERNAME'),
             password = os.environ.get('BW_PASSWORD')
@@ -44,46 +44,62 @@ class TestMessagesApi(unittest.TestCase):
         self.api_instance = messages_api.MessagesApi(api_client)
         self.account_id = os.environ.get('BW_ACCOUNT_ID')
 
+        # Unauthorized API Client
+        self.unauthorized_api_client = bandwidth.ApiClient()
+        self.unauthorized_api_instance = messages_api.MessagesApi(self.unauthorized_api_client)
 
-    def test_create_message(self):
-        message_request = MessageRequest(
-            application_id=os.environ.get('BW_MESSAGING_APPLICATION_ID'),
-            to=[os.environ.get('USER_NUMBER')],
-            _from=os.environ.get('BW_NUMBER'),
-            text="python integration",
-            media=["https://dev.bandwidth.com/images/bandwidth-logo.png","https://dev.bandwidth.com/images/github_logo.png"],
-            tag="python integration tag",
-            priority=PriorityEnum("default"),
+        # Message Properties
+        self.application_id = os.environ.get('BW_MESSAGING_APPLICATION_ID')
+        self.to_number = [os.environ.get('USER_NUMBER')]
+        self.from_number = os.environ.get('BW_NUMBER')
+        self.text = 'python integration'
+        self.media = ['https://cdn2.thecatapi.com/images/MTY3ODIyMQ.jpg']
+        self.tag = 'python integration tag'
+        self.priority = PriorityEnum("default")
+
+        # Message Request
+        self.message_request = MessageRequest(
+            application_id=self.application_id,
+            to=self.to_number,
+            _from=self.from_number,
+            text=self.text,
+            media=self.media,
+            tag=self.tag,
+            priority=self.priority,
         )
         
-        try:
-            api_response: Message = self.api_instance.create_message(self.account_id, message_request)
-        except bandwidth.ApiException as e:
-            print("Exception when calling MessagesApi->create_message: %s\n" % e)
-            
+        # Invalid Message Request
+        self.invalid_message_request = MessageRequest(
+            application_id=self.application_id,
+            to=['+invalid'],
+            _from='+invalid',
+            text='',
+        )
+
+
+    def test_create_message(self):
+        response: Message = self.api_instance.create_message(self.account_id, self.message_request, _return_http_data_only=False)
+
+        self.assertEqual(response[1], 202)
+
+        api_response = response[0]
         self.assertIsInstance(api_response, Message)
-        self.assertEqual(api_response.application_id,message_request.application_id)
-        self.assertEqual(api_response.to, message_request.to)
-        self.assertEqual(api_response._from, message_request._from)
-        self.assertEqual(api_response.owner, message_request._from)
-        self.assertEqual(api_response.text, message_request.text)
-        self.assertEqual(api_response.media, message_request.media)
-        self.assertEqual(api_response.tag, message_request.tag)
-        self.assertEqual(api_response.priority, message_request.priority)
+        self.assertEqual(api_response.application_id,self.application_id)
+        self.assertEqual(api_response.to, self.to_number)
+        self.assertEqual(api_response._from, self.from_number)
+        self.assertEqual(api_response.owner, self.from_number)
+        self.assertEqual(api_response.text, self.text)
+        self.assertEqual(api_response.media, self.media)
+        self.assertEqual(api_response.tag, self.tag)
+        self.assertIsInstance(api_response.priority, PriorityEnum)
+        self.assertEqual(api_response.priority, self.priority)
         self.assertEqual(api_response.segment_count, 1)
         self.assertTrue(datetime.fromisoformat(api_response.time[:-1]))
 
 
     def test_create_message_bad_request(self):
-        message_request = MessageRequest(
-            application_id=os.environ.get('BW_MESSAGING_APPLICATION_ID'),
-            to=['+invalid'],
-            _from='+invalid',
-            text='',
-        )
-        
         with self.assertRaises(ApiException) as context:
-            self.api_instance.create_message(self.account_id, message_request)
+            self.api_instance.create_message(self.account_id, self.invalid_message_request)
             
         self.assertEqual(context.exception.status, 400)
 
@@ -100,17 +116,8 @@ class TestMessagesApi(unittest.TestCase):
     
 
     def test_create_message_unauthorized(self):
-        api_client = bandwidth.ApiClient()
-        api_instance = messages_api.MessagesApi(api_client)
-        message_request = MessageRequest(
-            application_id=os.environ.get('BW_MESSAGING_APPLICATION_ID'),
-            to=['+invalid'],
-            _from='+invalid',
-            text='',
-        )
-        
         with self.assertRaises(UnauthorizedException) as context:
-            api_instance.create_message(self.account_id, message_request)
+            self.unauthorized_api_instance.create_message(self.account_id, self.invalid_message_request)
              
         self.assertEqual(context.exception.status, 401)
 
@@ -119,22 +126,29 @@ class TestMessagesApi(unittest.TestCase):
         self.assertEqual(e.type, 'unauthorized')
         self.assertEqual(e.description, 'Authentication Failed')
 
+    
+    @unittest.skip('The SDK catches incorrect content-type before making the request and attempts to create an ApiException,\
+                    but the creation of the exception fails since there is no response body. This should probably create some\
+                    kind of Client Exception instead, since this is not an actual API Exception.')
+    def test_create_message_invalid_media(self):
+        with self.assertRaises(ApiException) as context:
+            self.api_instance.create_message(self.account_id, self.message_request, _content_type='application/xml')
+
 
     def test_list_messages(self):
         message_direction = ListMessageDirectionEnum("OUTBOUND")
         
-        try:
-            api_response = self.api_instance.list_messages(self.account_id, message_direction=message_direction)
-        except bandwidth.ApiException as e:
-            print("Exception when calling MessagesApi->list_messages: %s\n" % e)
+        response = self.api_instance.list_messages(self.account_id, message_direction=message_direction, _return_http_data_only=False)
 
+        self.assertEqual(response[1], 200)
+
+        api_response = response[0]
         self.assertIsInstance(api_response, MessagesList)
         self.assertGreater(api_response.total_count, 0)
         self.assertTrue(api_response.messages[0], ListMessageItem)
         
         message = api_response.messages[0]
-        self.assertEqual(message.account_id, os.environ.get('BW_ACCOUNT_ID'))
-        self.assertTrue(message.carrier_name)
+        self.assertEqual(message.account_id, self.account_id)
         self.assertRegex(message.destination_tn, '^\\+[1-9]\\d{1,14}$')
         self.assertFalse(message.error_code)
         self.assertIsInstance(message.message_direction, ListMessageDirectionEnum)
@@ -160,11 +174,9 @@ class TestMessagesApi(unittest.TestCase):
 
     
     def test_list_messages_unauthorized(self):
-        api_client = bandwidth.ApiClient()
-        api_instance = messages_api.MessagesApi(api_client)
         
         with self.assertRaises(UnauthorizedException) as context:
-            api_instance.list_messages(self.account_id)
+            self.unauthorized_api_instance.list_messages(self.account_id)
              
         self.assertEqual(context.exception.status, 401)
 
