484052f13390adb4f6c7e82b51ff1f3a9baeac3f
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-09-13 16:29:47 -0400

    Made it fancier

--- bandwidth/utilities/web_rtc.py
+++ bandwidth/utilities/web_rtc.py
@@ -2,7 +2,29 @@ from bandwidth.model.bxml.response import Response
 from bandwidth.model.bxml.verbs import *
 
 
-def generate_transfer_verb(device_token: str, voice_call_id: str, sip_uri: str = 'sip:sipx.webrtc.bandwidth.com:5060') -> str:
+def _generate_transfer_verb(device_token: str, voice_call_id: str, sip_uri: str = 'sip:sipx.webrtc.bandwidth.com:5060') -> Transfer:
+    """Generate a Transfer object for a WebRTC Session
+
+    Args:
+        device_token (str): The device token.
+        voice_call_id (str): The Bandwidth voice Call ID.
+        sip_uri (str, optional): The SIP URI to transfer the call to. Defaults to 'sip:sipx.webrtc.bandwidth.com:5060'.
+
+    Returns:
+        Transfer: Returns a Transfer BXML Verb Object
+    """
+    uui = "".join(voice_call_id.split("-")[1::])
+    sip_uri = SipUri(
+        uui=f"{uui};encoding=base64,{device_token};encoding=jwt",
+        uri=sip_uri
+    )
+    transfer = Transfer(
+        sip_uris=[sip_uri]
+    )
+    return transfer
+
+
+def generate_transfer_bxml(device_token: str, voice_call_id: str, sip_uri: str = 'sip:sipx.webrtc.bandwidth.com:5060') -> str:
     """Returns the Transfer verb to perform the SIP transfer without the Response wrapper
 
     Args:
@@ -13,19 +35,10 @@ def generate_transfer_verb(device_token: str, voice_call_id: str, sip_uri: str =
     Returns:
         str: <Transfer> BXML Verb
     """
-    uui = "".join(voice_call_id.split("-")[1::])
-    sip_uri = SipUri(
-        uui=f"{uui};encoding=base64,{device_token};encoding=jwt",
-        uri=sip_uri
-    )
-    transfer = Transfer(
-        sip_uris=[sip_uri]
-
-    )
-    return transfer.to_bxml()
+    return _generate_transfer_verb(device_token, voice_call_id, sip_uri).to_bxml()
 
 
-def generate_transfer_bxml(device_token: str, voice_call_id: str, sip_uri: str = 'sip:sipx.webrtc.bandwidth.com:5060') -> str:
+def generate_transfer_bxml_document(device_token: str, voice_call_id: str, sip_uri: str = 'sip:sipx.webrtc.bandwidth.com:5060') -> str:
     """Generate BXML document with WebRTC a device token to perform a SIP transfer
 
     Args:
@@ -36,14 +49,6 @@ def generate_transfer_bxml(device_token: str, voice_call_id: str, sip_uri: str =
     Returns:
         str: BXML document with transfer BXML
     """
-    uui = "".join(voice_call_id.split("-")[1::])
-    transfer_bxml = Response()
-    sip_uri = SipUri(
-        uui=f"{uui};encoding=base64,{device_token};encoding=jwt",
-        uri=sip_uri
-    )
-    transfer = Transfer(
-        sip_uris=[sip_uri]
-    )
-    transfer_bxml.add_verb(transfer)
-    return transfer_bxml.to_bxml()
+    response = Response()
+    response.add_verb(_generate_transfer_verb(device_token, voice_call_id, sip_uri))
+    return response.to_bxml()
--- test/unit/test_webrtc_utilities.py
+++ test/unit/test_webrtc_utilities.py
@@ -7,13 +7,13 @@ class TestWebRtcUtilities:
     """
 
     def test_generate_transfer_bxml(self):
-        expected = '<?xml version="1.0" encoding="UTF-8"?><Response><Transfer><SipUri uui="93d6f3c0be5845960b744fa28015d8ede84bd1a4;encoding=base64,asdf;encoding=jwt">sip:sipx.webrtc.bandwidth.com:5060</SipUri></Transfer></Response>'
+        expected = '<Transfer><SipUri uui="93d6f3c0be5845960b744fa28015d8ede84bd1a4;encoding=base64,asdf;encoding=jwt">sip:sipx.webrtc.bandwidth.com:5060</SipUri></Transfer>'
         actual = generate_transfer_bxml(
             'asdf', 'c-93d6f3c0-be584596-0b74-4fa2-8015-d8ede84bd1a4')
         assert actual == expected
 
-    def test_generate_transfer_verb(self):
-        expected = '<Transfer><SipUri uui="93d6f3c0be5845960b744fa28015d8ede84bd1a4;encoding=base64,asdf;encoding=jwt">sip:sipx.webrtc.bandwidth.com:5060</SipUri></Transfer>'
-        actual = generate_transfer_verb(
+    def test_generate_transfer_bxml_document(self):
+        expected = '<?xml version="1.0" encoding="UTF-8"?><Response><Transfer><SipUri uui="93d6f3c0be5845960b744fa28015d8ede84bd1a4;encoding=base64,asdf;encoding=jwt">sip:sipx.webrtc.bandwidth.com:5060</SipUri></Transfer></Response>'
+        actual = generate_transfer_bxml_document(
             'asdf', 'c-93d6f3c0-be584596-0b74-4fa2-8015-d8ede84bd1a4')
         assert actual == expected
