b8ecf1a2288090e7a31a8548b3e6316af0b4b9a8
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-09-15 16:30:04 -0400

    DX-2849 Add `webrtc_utils` (#110)
    * DX-2849 Add `webrtc_utils`
    
    Add WebRTC Utilities module with helper methods to generate transfer BXML
    
    * Made it fancier
    
    * Refactor function names

---
+++ bandwidth/utilities/web_rtc.py
@@ -0,0 +1,54 @@
+from bandwidth.model.bxml.response import Response
+from bandwidth.model.bxml.verbs import *
+
+
+def _generate_transfer_model(device_token: str, voice_call_id: str, sip_uri: str = 'sip:sipx.webrtc.bandwidth.com:5060') -> Transfer:
+    """Generate a Transfer object for a WebRTC Session
+
+    Args:
+        device_token (str): The device token.
+        voice_call_id (str): The Bandwidth voice Call ID.
+        sip_uri (str, optional): The SIP URI to transfer the call to. Defaults to 'sip:sipx.webrtc.bandwidth.com:5060'.
+
+    Returns:
+        Transfer: Returns a Transfer BXML Verb Object
+    """
+    uui = "".join(voice_call_id.split("-")[1::])
+    sip_uri = SipUri(
+        uui=f"{uui};encoding=base64,{device_token};encoding=jwt",
+        uri=sip_uri
+    )
+    transfer = Transfer(
+        sip_uris=[sip_uri]
+    )
+    return transfer
+
+
+def generate_transfer_bxml_verb(device_token: str, voice_call_id: str, sip_uri: str = 'sip:sipx.webrtc.bandwidth.com:5060') -> str:
+    """Returns the Transfer verb to perform the SIP transfer without the Response wrapper
+
+    Args:
+        device_token (str): The device token.
+        voice_call_id (str): The Bandwidth voice Call ID.
+        sip_uri (str, optional): The SIP URI to transfer the call to. Defaults to 'sip:sipx.webrtc.bandwidth.com:5060'.
+
+    Returns:
+        str: <Transfer> BXML Verb
+    """
+    return _generate_transfer_model(device_token, voice_call_id, sip_uri).to_bxml()
+
+
+def generate_transfer_bxml(device_token: str, voice_call_id: str, sip_uri: str = 'sip:sipx.webrtc.bandwidth.com:5060') -> str:
+    """Generate BXML document with WebRTC a device token to perform a SIP transfer
+
+    Args:
+        device_token (str): The device token.
+        voice_call_id (str): The Bandwidth voice Call ID.
+        sip_uri (str, optional): The SIP URI to transfer the call to. Defaults to 'sip:sipx.webrtc.bandwidth.com:5060'.
+
+    Returns:
+        str: BXML document with transfer BXML
+    """
+    response = Response()
+    response.add_verb(_generate_transfer_model(device_token, voice_call_id, sip_uri))
+    return response.to_bxml()
---
+++ test/unit/test_webrtc_utilities.py
@@ -0,0 +1,19 @@
+from bandwidth.utilities.web_rtc import *
+
+
+class TestWebRtcUtilities:
+    """
+    Class for the WebRTC Utilities Tests
+    """
+
+    def test_generate_transfer_bxml(self):
+        expected = '<Transfer><SipUri uui="93d6f3c0be5845960b744fa28015d8ede84bd1a4;encoding=base64,asdf;encoding=jwt">sip:sipx.webrtc.bandwidth.com:5060</SipUri></Transfer>'
+        actual = generate_transfer_bxml_verb(
+            'asdf', 'c-93d6f3c0-be584596-0b74-4fa2-8015-d8ede84bd1a4')
+        assert actual == expected
+
+    def test_generate_transfer_bxml_document(self):
+        expected = '<?xml version="1.0" encoding="UTF-8"?><Response><Transfer><SipUri uui="93d6f3c0be5845960b744fa28015d8ede84bd1a4;encoding=base64,asdf;encoding=jwt">sip:sipx.webrtc.bandwidth.com:5060</SipUri></Transfer></Response>'
+        actual = generate_transfer_bxml(
+            'asdf', 'c-93d6f3c0-be584596-0b74-4fa2-8015-d8ede84bd1a4')
+        assert actual == expected
