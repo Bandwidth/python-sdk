5df7c57b381f166ca20cafeae7065d7553ec40dd
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-09-28 13:59:28 -0400

    Fix other `List` instances

--- bandwidth/model/bxml/bxml.py
+++ bandwidth/model/bxml/bxml.py
@@ -5,12 +5,14 @@ Class that allows user to generate a Bxml document
 
 @copyright Bandwidth INC
 """
+from typing import List
+
 from .root import Root
 from .verb import Verb
 
 
 class Bxml(Root):
-    def __init__(self, nested_verbs: list[Verb] = []):
+    def __init__(self, nested_verbs: List[Verb] = []):
         """Initialize an instance of the <Bxml> root
 
         Args:
