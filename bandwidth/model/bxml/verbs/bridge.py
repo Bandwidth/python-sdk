8bd310d958f27aff93d22557ef52228bccf198d6
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-09-29 17:00:51 -0400

    Finish `<Bridge>` and test

--- bandwidth/model/bxml/verbs/bridge.py
+++ bandwidth/model/bxml/verbs/bridge.py
@@ -8,11 +8,60 @@ Bandwidth's Bridge BXML verb
 from ..verb import Verb
 
 
-class Tag(Verb):
+class Bridge(Verb):
 
-    # TODO: Finish adding attributes
-    def __init__(self, target_call, bridge_complete_url=None, bridge_complete_method=None):
-        super().__init__(tag="Bridge", content=target_call, attributes=None, nested_verbs=None)
-    
+    def __init__(
+        self, target_call: str, bridge_complete_url: str=None,
+        bridge_complete_method: str=None,
+        bridge_complete_fallback_url: str=None,
+        bridge_complete_fallback_method: str=None,
+        bridge_target_complete_url: str=None,
+        bridge_target_complete_method: str=None,
+        bridge_target_complete_fallback_url: str=None,
+        bridge_target_complete_fallback_method: str=None,
+        username: str=None, password: str=None,
+        fallback_username: str=None, fallback_password: str=None,
+        tag: str=None
+    ):
+        """Initialize a <Bridge> verb
+
+        Args:
+            target_call (str): _description_
+            bridge_complete_url (str, optional): _description_. Defaults to None.
+            bridge_complete_method (str, optional): _description_. Defaults to None.
+            bridge_complete_fallback_url (str, optional): _description_. Defaults to None.
+            bridge_complete_fallback_method (str, optional): _description_. Defaults to None.
+            bridge_target_complete_url (str, optional): _description_. Defaults to None.
+            bridge_target_complete_method (str, optional): _description_. Defaults to None.
+            bridge_target_complete_fallback_url (str, optional): _description_. Defaults to None.
+            bridge_target_complete_fallback_method (str, optional): _description_. Defaults to None.
+            username (str, optional): _description_. Defaults to None.
+            password (str, optional): _description_. Defaults to None.
+            fallback_username (str, optional): _description_. Defaults to None.
+            fallback_password (str, optional): _description_. Defaults to None.
+            tag (str, optional): _description_. Defaults to None.
+        """
+        self.attributes = {
+            "bridgeCompleteUrl": bridge_complete_url,
+            "bridgeCompleteMethod": bridge_complete_method,
+            "bridgeCompleteFallbackUrl": bridge_complete_fallback_url,
+            "bridgeCompleteFallbackMethod": bridge_complete_fallback_method,
+            "bridgeTargetCompleteUrl": bridge_target_complete_url,
+            "bridgeTargetCompleteMethod": bridge_target_complete_method,
+            "bridgeTargetCompleteFallback_url": bridge_target_complete_fallback_url,
+            "bridgeTargetCompleteFallbackMethod": bridge_target_complete_fallback_method,
+            "username": username,
+            "password": password,
+            "fallbackUsername": fallback_username,
+            "fallbackUassword": fallback_password,
+            "tag": tag
+        }
+        super().__init__(
+            tag="Bridge",
+            content=target_call,
+            attributes=self.attributes,
+            nested_verbs=None
+        )
+
     def add_verb(self, verb: Verb):
         raise AttributeError('Adding verbs is not supported by <Bridge>')
