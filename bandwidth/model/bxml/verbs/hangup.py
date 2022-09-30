5e80c0bfc5f9409dee105a6e31ba7d5d8660236c
Brian <bgome004@gmail.com>
2022-09-30 11:10:01 -0400

    Update hangup.py

--- bandwidth/model/bxml/verbs/hangup.py
+++ bandwidth/model/bxml/verbs/hangup.py
@@ -16,4 +16,4 @@ class Hangup(TerminalVerb):
         Args:
             None
         """
-        super().__init__(tag="Hangup", content=None, attributes=None, nested_verbs=None)
+        super().__init__(tag="Hangup", content=None, attributes=None)
