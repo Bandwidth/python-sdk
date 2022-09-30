310b848bae495f9b315bd2d2e4978be249c00374
Brian <bgome004@gmail.com>
2022-09-30 15:51:30 -0400

    Refactored due to addition of terminal verb

--- bandwidth/model/bxml/verbs/pause.py
+++ bandwidth/model/bxml/verbs/pause.py
@@ -3,8 +3,8 @@ pause.py
 Bandwidth's Pause BXML verb
 @copyright Bandwidth INC
 """
-from ..verb import Verb
-class Pause(Verb):
+from ..terminal_verb import TerminalVerb
+class Pause(TerminalVerb):
     def __init__(self, duration:str=None):
         """Initialize a <Pause> verb
         Args:
@@ -14,13 +14,4 @@ class Pause(Verb):
         "duration": duration
         }
 
-        super().__init__(tag="Pause", content=None, attributes=self.attributes, nested_verbs=None)
-
-    def add_verb(self, verb: Verb):
-        """Adding verbs is not allowed for <Pause>
-        Args:
-            verb (Verb): BXML verb
-        Raises:
-            AttributeError: This method is not allowed for <Pause>
-        """
-        raise AttributeError('Adding verbs is not supported by <Pause>')
+        super().__init__(tag="Pause", content=None, attributes=self.attributes)
