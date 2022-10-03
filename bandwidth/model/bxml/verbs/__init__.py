eefd4d8d6b3b45dea32f5ead1b22119b99b79c68
Brian <bgome004@gmail.com>
2022-10-03 13:58:35 -0400

    DX-2915 StopRecording Refactor

--- bandwidth/model/bxml/verbs/__init__.py
+++ bandwidth/model/bxml/verbs/__init__.py
@@ -2,5 +2,6 @@ from .bridge import Bridge
 from .phone_number import PhoneNumber
 from .record import Record
 from .sip_uri import SipUri
+from .stop_recording import StopRecording
 from .tag import Tag
 from .transfer import Transfer
