1051f728613011a9c0d71c39c902cf84d1b3555d
Brian <bgome004@gmail.com>
2022-10-04 16:46:47 -0400

    Update conference.py

--- bandwidth/model/bxml/verbs/conference.py
+++ bandwidth/model/bxml/verbs/conference.py
@@ -18,8 +18,8 @@ from .resume_recording import ResumeRecording
 class Conference(Verb):
 
     def __init__(
-        self, audio_and_recording_verbs: List[Union[PlayAudio, SpeakSentence, StartRecording, StopRecording, PauseRecording, ResumeRecording]] = [],
-        name: str, mute: str=None, 
+        self, name: str,
+        audio_and_recording_verbs: List[Union[PlayAudio, SpeakSentence, StartRecording, StopRecording, PauseRecording, ResumeRecording]] = [], mute: str=None, 
         hold: str=None, call_ids_to_coach: str=None, 
         conference_event_url: str=None, conference_event_method: str=None, 
         conference_event_fallback_url: str=None, conference_event_fallback_method: str=None, 
