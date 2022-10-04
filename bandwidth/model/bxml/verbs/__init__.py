efea798b245e9e6a81a2646bffc71a78c99b3d59
Brian <bgome004@gmail.com>
2022-10-04 14:09:50 -0400

    DX-2910

--- bandwidth/model/bxml/verbs/__init__.py
+++ bandwidth/model/bxml/verbs/__init__.py
@@ -3,5 +3,6 @@ from .pause_recording import PauseRecording
 from .phone_number import PhoneNumber
 from .record import Record
 from .sip_uri import SipUri
+from .start_gather import StartGather
 from .tag import Tag
 from .transfer import Transfer
