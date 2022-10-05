8e1194b30400da9de17f4df1d8b59296644ab4c7
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-10-05 14:11:07 -0400

    Add param descriptions for `bridge.py` constructor

--- bandwidth/model/bxml/verbs/bridge.py
+++ bandwidth/model/bxml/verbs/bridge.py
@@ -26,20 +26,24 @@ class Bridge(TerminalVerb):
         """Initialize a <Bridge> verb
 
         Args:
-            target_call (str): _description_
-            bridge_complete_url (str, optional): _description_. Defaults to None.
-            bridge_complete_method (str, optional): _description_. Defaults to None.
-            bridge_complete_fallback_url (str, optional): _description_. Defaults to None.
-            bridge_complete_fallback_method (str, optional): _description_. Defaults to None.
-            bridge_target_complete_url (str, optional): _description_. Defaults to None.
-            bridge_target_complete_method (str, optional): _description_. Defaults to None.
-            bridge_target_complete_fallback_url (str, optional): _description_. Defaults to None.
-            bridge_target_complete_fallback_method (str, optional): _description_. Defaults to None.
-            username (str, optional): _description_. Defaults to None.
-            password (str, optional): _description_. Defaults to None.
-            fallback_username (str, optional): _description_. Defaults to None.
-            fallback_password (str, optional): _description_. Defaults to None.
-            tag (str, optional): _description_. Defaults to None.
+            target_call (str): String containing the callId of the call to be bridged.
+            bridge_complete_url (str, optional): URL to send the Bridge Complete event to and request new BXML.
+                If this attribute is specified, then Verbs following the <Bridge> verb will be ignored and the BXML returned in this webhook is executed on the call.
+                If this attribute is not specified then no webhook will be sent, and execution of the verbs following the <Bridge> verb continues. May be a relative URL. Defaults to None.
+            bridge_complete_method (str, optional): The HTTP method to use for the request to bridgeCompleteUrl. GET or POST. Default value is POST.
+            bridge_complete_fallback_url (str, optional): A fallback url which, if provided, will be used to retry the Bridge Complete webhook delivery in case bridgeCompleteUrl fails to respond. Defaults to None.
+            bridge_complete_fallback_method (str, optional): The HTTP method to use to deliver the Bridge Complete webhook to bridgeCompleteFallbackUrl. GET or POST. Default value is POST.
+            bridge_target_complete_url (str, optional):URL to send the Bridge Target Complete event to and request new BXML.
+                If this attribute is specified, then the BXML returned in this webhook is executed on the target call.
+                If this attribute is not specified then no webhook will be sent, and the target call will be hung up. May be a relative URL. Defaults to None.
+            bridge_target_complete_method (str, optional): The HTTP method to use for the request to bridgeTargetCompleteUrl. GET or POST. Default value is POST.
+            bridge_target_complete_fallback_url (str, optional): A fallback url which, if provided, will be used to retry the Bridge Target Complete webhook delivery in case bridgeTargetCompleteUrl fails to respond. Defaults to None.
+            bridge_target_complete_fallback_method (str, optional): The HTTP method to use to deliver the Bridge Target Complete webhook to bridgeTargetCompleteFallbackUrl. GET or POST. Default value is POST.
+            username (str, optional): The username to send in the HTTP request to bridgeCompleteUrl and to bridgeTargetCompleteUrl. Defaults to None.
+            password (str, optional): The password to send in the HTTP request to bridgeCompleteUrl and to bridgeTargetCompleteUrl. Defaults to None.
+            fallback_username (str, optional): The username to send in the HTTP request to bridgeCompleteFallbackUrl and to bridgeTargetCompleteFallbackUrl. Defaults to None.
+            fallback_password (str, optional): The password to send in the HTTP request to bridgeCompleteFallbackUrl and to bridgeTargetCompleteFallbackUrl. Defaults to None.
+            tag (str, optional): A custom string that will be sent with the bridgeComplete webhook and all future webhooks of the call unless overwritten by a future tag attribute or <Tag> verb, or cleared. May be cleared by setting tag="". Max length 256 characters. Defaults to None.
         """
         self.target_call = target_call
         self.bridge_complete_url = bridge_complete_url
