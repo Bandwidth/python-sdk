997a555d1267b38c9799e6e14bd7b3d6002da53e
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-10-13 09:13:01 -0400

    Set pause duration to be Int

--- bandwidth/model/bxml/verbs/pause.py
+++ bandwidth/model/bxml/verbs/pause.py
@@ -7,15 +7,15 @@ Bandwidth's Pause BXML verb
 """
 from ..terminal_verb import TerminalVerb
 class Pause(TerminalVerb):
-    def __init__(self, duration:str=None):
+    def __init__(self, duration:int=1):
         """Initialize a <Pause> verb
         Args:
             duration (str, optional): The time in seconds to pause. Default value is 1.
         """
-        self.duration = duration
+        self.duration = str(duration)
 
         super().__init__(tag="Pause")
-    
+
     @property
     def _attributes(self):
         return {
