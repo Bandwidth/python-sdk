c9d4fd910c6f9d8553ca0d1215403e1ff21efed5
ckoegel <ckoegel1006@gmail.com>
2025-09-04 16:12:47 -0400

    SWI-8574 Add `wait` to `StopStream` Verb

--- test/unit/models/bxml/test_stop_stream.py
+++ test/unit/models/bxml/test_stop_stream.py
@@ -13,12 +13,12 @@ from bandwidth.models.bxml import StopStream, Verb
 class TestStopStream(unittest.TestCase):
 
     def setUp(self):
-        self.stop_stream = StopStream(name="conf")
+        self.stop_stream = StopStream(name="conf", wait=True)
 
     def test_instance(self):
         assert isinstance(self.stop_stream, StopStream)
         assert isinstance(self.stop_stream, Verb)
 
     def test_to_bxml(self):
-        expected = '<StopStream name="conf" />'
+        expected = '<StopStream name="conf" wait="True" />'
         assert expected == self.stop_stream.to_bxml()
