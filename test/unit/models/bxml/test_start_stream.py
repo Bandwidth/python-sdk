3eae30ee49619c5178ec4856b392ca6f1cb4ecc1
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2025-09-30 14:50:12 -0400

    VAPI-2528 Fix Start Stream BXML Bug (#265)

--- test/unit/models/bxml/test_start_stream.py
+++ test/unit/models/bxml/test_start_stream.py
@@ -43,10 +43,10 @@ class TestStartStream(unittest.TestCase):
         assert isinstance(self.start_stream, Verb)
 
     def test_to_bxml(self):
-        expected = '<StartStream destination="testurl.com" destination_username="dest_user" destination_password="dest_pass" name="stream1" mode="bidirectional" tracks="inbound" streamEventUrl="eventurl.com" streamEventMethod="POST" username="user" password="pass"><StreamParam name="name1" value="value1" /></StartStream>'
+        expected = '<StartStream destination="testurl.com" destinationUsername="dest_user" destinationPassword="dest_pass" name="stream1" mode="bidirectional" tracks="inbound" streamEventUrl="eventurl.com" streamEventMethod="POST" username="user" password="pass"><StreamParam name="name1" value="value1" /></StartStream>'
         assert expected == self.start_stream.to_bxml()
 
     def test_add_verb(self):
-        expected = '<StartStream destination="testurl.com" destination_username="dest_user" destination_password="dest_pass" name="stream1" mode="bidirectional" tracks="inbound" streamEventUrl="eventurl.com" streamEventMethod="POST" username="user" password="pass"><StreamParam name="name1" value="value1" /><StreamParam name="name2" value="value2" /></StartStream>'
+        expected = '<StartStream destination="testurl.com" destinationUsername="dest_user" destinationPassword="dest_pass" name="stream1" mode="bidirectional" tracks="inbound" streamEventUrl="eventurl.com" streamEventMethod="POST" username="user" password="pass"><StreamParam name="name1" value="value1" /><StreamParam name="name2" value="value2" /></StartStream>'
         self.start_stream.add_verb(self.stream_param2)
         assert expected == self.start_stream.to_bxml()
